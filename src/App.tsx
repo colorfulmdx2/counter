9528cadb1207cc8776cf6daa13447a127aa2f5e1
colorfulmdx2 <colorfulmdx2@gmail.com>
2020-05-29 21:54:02 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import Counter from "./counter/Counter";
+import Settings from "./setting/Settings";
 
 function App() {
+
+  const maxValue = 5
+
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    if(maxValue === count) {
+      return
+    }
+    setCount(count + 1)
+  }
+  const reset = () => setCount(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='content'>
+          <div className='settings'>
+              <Settings />
+          </div>
+          <div className='counter'>
+              <Counter increment={increment}
+                       count={count}
+                       reset={reset}
+                       maxValue={maxValue}
+
+              />
+        </div>
+
+      </div>
+
+  )
+
 }
 
 export default App;
